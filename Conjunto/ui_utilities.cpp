a57b8196fc6fe9d5983c4fa5cab98dc712511064
pedrofixe <yurbypedro@hotmail.com>
2017-04-28 12:56:40 +0100

    Acabamento de ui_utilities

--- Conjunto/ui_utilities.cpp
+++ Conjunto/ui_utilities.cpp
@@ -1,19 +1,10 @@
-// Alterar as variaveis GlobalWidth e GlobalHeight para o valor pretendido
-// Se nao pretender alterar o tamanho da janela alterar a variavel ChangeWindowSize para 0
-
-#include <iostream>
-#include <string>
 #include "ui_utilities.h"
-
-//int GlobalWidth = 130; // 120 ate 240
-//int GlobalHeight = 30; // 30 ate 63
-bool ChangeWindowSize = 1;
 
 #ifdef _WIN32
 
 #include <windows.h>
 
-void ClearScreen()
+void ui_utilities::ClearScreen()
 {
 	HANDLE                     hStdOut;
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -50,66 +41,60 @@ void ClearScreen()
 			SetConsoleCursorPosition(hStdOut, homeCoords);
 	}
 
-	bool SetWindow(int Width, int Height)
+	bool ui_utilities::SetWindow(int Width, int Height)
 	{
-		if (ChangeWindowSize)
-		{
-			_COORD coord;
-			coord.X = Width;
-			coord.Y = Height;
+		_COORD coord;
+		coord.X = Width;
+		coord.Y = Height;
 
-			_SMALL_RECT Rect;
-			Rect.Top = 0;
-			Rect.Left = 0;
-			Rect.Bottom = Height - 1;
-			Rect.Right = Width - 1;
+		_SMALL_RECT Rect;
+		Rect.Top = 0;
+		Rect.Left = 0;
+		Rect.Bottom = Height - 1;
+		Rect.Right = Width - 1;
 
 		// Get handle of the standard output 
-			HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
-			if (Handle == NULL)
-			{
-				cout << "Failure in getting the handle\n" << GetLastError();
-				return FALSE;
-			}
+		HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (Handle == NULL)
+		{
+			cout << "Failure in getting the handle\n" << GetLastError();
+			return FALSE;
+		}
 
 		// Set screen buffer size to that specified in coord 
-			if (!SetConsoleScreenBufferSize(Handle, coord))
-			{
-				cout << "Failure in setting buffer size\n" << GetLastError();
-				return FALSE;
-			}
+		if (!SetConsoleScreenBufferSize(Handle, coord))
+		{
+			cout << "Failure in setting buffer size\n" << GetLastError();
+			return FALSE;
+		}
 
 		// Set the window size to that specified in Rect 
-			if (!SetConsoleWindowInfo(Handle, TRUE, &Rect))
-			{
-				cout << "Failure in setting window size\n" << GetLastError();
-				return FALSE;
-			}
+		if (!SetConsoleWindowInfo(Handle, TRUE, &Rect))
+		{
+			cout << "Failure in setting window size\n" << GetLastError();
+			return FALSE;
 		}
-		return TRUE;
-
 	}
+	return TRUE;
 
 #else
 
-	void ClearScreen()
+	void ui_utilities::ClearScreen()
 	{
 		std::cout << "\033[2J\033[1;1H";
 		std::cout << "\033[2J\033[1;1H";
 	}
 
-	bool SetWindow(int width, int height)
+	bool ui_utilities::SetWindow(int width, int height)
 	{
-		if (ChangeWindowSize)
-		{
-			std::string res = "\e[8;";
-			res += std::to_string(height);
-			res += ";";
-			res += std::to_string(width);
-			res += "t";
-			std::cout << res;
+		std::string res = "\e[8;";
+		res += std::to_string(height);
+		res += ";";
+		res += std::to_string(width);
+		res += "t";
+		std::cout << res;
 
-		}
+		
 		return true;
 	}
 
--- Conjunto/ui_utilities.h
+++ Conjunto/ui_utilities.h
@@ -1,7 +1,17 @@
 #ifndef __UI_UTILITIES_INCLUDED__
 #define __UI_UTILITIES_INCLUDED__
 
-void ClearScreen();
-bool SetWindow(int, int);
+#include <iostream>
+#include <string>
+
+class ui_utilities{
+
+public:
+	
+	static void ClearScreen();
+	static bool SetWindow(int, int);
+
+};
+
 
 #endif
