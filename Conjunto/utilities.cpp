958169da84f5ae0f2e044f54ba2676c5a2de0355
pedrofixe <yurbypedro@hotmail.com>
2017-04-28 17:26:28 +0100

    Made utilities header to complement other headers with fucntions like trimstring

---
+++ Conjunto/utilities.cpp
@@ -0,0 +1,35 @@
+#include "utilities.h"
+
+template <class T>
+
+//Swap the value of two integers
+void swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+// Remove spaces at the beginning and at the end of a string
+void TrimString(std::string &input)
+{
+	while (input[0] == ' ')
+		input.erase(input.begin());
+
+	while (input.back() == ' ')
+		input.erase(input.end() - 1);
+}
+
+// Caluclates the length of a string
+int intsize(int input)
+{
+	int res = 0;
+
+	while (input)
+	{
+		res++;
+		input /= 10;
+	}
+
+	return res;
+}
---
+++ Conjunto/utilities.h
@@ -0,0 +1,17 @@
+#ifndef __UTILITIES_INCLUDED__
+#define __UTILITIES_INCLUDED__
+
+#include <string>
+
+class utilities{
+
+public:
+	template <class T>
+
+	static void swap(T, T);
+	static void trimstring(string);
+	static int intsize(int);
+};
+
+
+#endif
